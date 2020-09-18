11a8e0c71f89c69b12fd6c0565ba0ee7282bc6c5
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-18 18:29:33 +0800

    initial desgin of navbar done

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MDBContainer, MDBNavbar, 
+    MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, 
+    MDBCollapse, MDBNavItem, MDBIcon } from 'mdbreact';
 import Link from 'next/link';
 
 const Navbar = () => {
-    return (
-        <div>
-            <div className="p-4 d-flex justify-content-between black-all">
-                <Link href="/">Home</Link>
-                <Link href="/about">About</Link>
-                <Link href="/blog">Blog</Link>
-                <Link href="/contact">Contact</Link>
-            </div>
-        </div>
-    )
+    const [collapse, setCollapse] = useState(false);
+
+    const onClick = () => setCollapse(!collapse)
+
+    return(
+      <div className="mb-5 pb-5">
+          <header>
+            <MDBNavbar dark expand="md" scrolling fixed="top">
+              <Link href="/">
+                  <img src="/assets/images/logo.png" className="navbar-logo" alt=""/>
+              </Link>
+              <MDBNavbarToggler onClick={()=>onClick} />
+              <MDBCollapse isOpen={collapse} navbar>
+                <MDBNavbarNav right>
+                  <MDBNavItem>
+                    <Link href="/about">
+                        <a className="h4-responsive">
+                            About
+                        </a>
+                    </Link>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <Link href="/blog">
+                        <a className="h4-responsive">
+                            Blog
+                        </a>
+                    </Link>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <Link href="/shop">
+                        <a className="h4-responsive">
+                            Shop
+                        </a>
+                    </Link>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <Link href="/contact">
+                        <a className="h4-responsive">
+                            Contact
+                        </a>
+                    </Link>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+                <MDBNavbarNav right>
+                  <MDBNavItem>
+                    <Link href="/fb"><MDBIcon fab size="lg" icon="facebook-f" /></Link>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <Link href="/twt"><MDBIcon fab size="lg" icon="twitter" /></Link>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <Link href="/ig"><MDBIcon fab size="lg" icon="instagram" /></Link>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBNavbar>
+          </header>
+      </div>
+    );
 }
 
-export default Navbar
+export default Navbar;
