e407b2257c115ef61ad9889d866098cd4ccd21c6
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-18 18:36:31 +0800

    added seo

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,17 @@
+import { MDBContainer } from 'mdbreact'
 import React from 'react'
+import Layout from '../components/Layout'
+import SEO from '../components/SEO'
 
 const MissingPage = () => {
     return (
-        <div>
-            
-        </div>
+        <Layout>
+            <SEO title="Page Not Found" 
+            desc="Error code 404, we coulnd't find what you we're looking for. Sorry for the Inconvenience."/>
+            <MDBContainer>
+                
+            </MDBContainer>
+        </Layout>
     )
 }
 
