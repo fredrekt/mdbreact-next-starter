2123384fd2d124c4017f645a46f729e455d1692a
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-19 07:21:02 +0800

    404 page done

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,4 @@
-import { MDBContainer } from 'mdbreact'
+import { MDBCol, MDBContainer, MDBRow } from 'mdbreact'
 import React from 'react'
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
@@ -8,8 +8,18 @@ const MissingPage = () => {
         <Layout>
             <SEO title="Page Not Found" 
             desc="Error code 404, we coulnd't find what you we're looking for. Sorry for the Inconvenience."/>
-            <MDBContainer>
-                
+            <MDBContainer className="py-5">
+                <MDBRow className="align-items-center py-5">
+                    <MDBCol md="6" lg="6">
+                        <div className="content text-center">
+                            <h1 className="h1-responsive">Oooopssieee</h1>
+                            <p className="lead">Page not found. Sorry for the Inconvenience, we're not able to find what you were looking for.</p>
+                        </div>
+                    </MDBCol>
+                    <MDBCol md="6" lg="6">
+                        <img src="/assets/images/missing.png" alt="" className="w-100"/>
+                    </MDBCol>
+                </MDBRow>
             </MDBContainer>
         </Layout>
     )
