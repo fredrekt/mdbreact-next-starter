2a29cddc30bb41743599b61d2baa37d818edc2d2
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-19 07:23:27 +0800

    contact page set

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,4 @@
-import { MDBContainer } from 'mdbreact'
+import { MDBCol, MDBContainer, MDBRow, MDBInput, MDBBtn } from 'mdbreact'
 import React from 'react'
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
@@ -8,8 +8,19 @@ const ContactPage = () => {
         <Layout>  
             <SEO title="Contact" 
             desc="Send us a message here. Find out where we are located"/>
-            <MDBContainer>
-                <h1>Hello Contact</h1>
+            <MDBContainer className="py-5">
+                <MDBRow className="py-4">
+                    <MDBCol md="6" lg="6">
+                        <h1 className="h1-responsive">Let's Talk</h1>
+                        <form action="">
+                            <MDBInput label="Your Name *" />
+                            <MDBInput label="Your Email *" />
+                            <MDBInput label="Your Message *" />
+                            <MDBInput type="textarea" label="Your Message *" rows="5" />
+                            <MDBBtn color="dark" className="mx-0 text-capitalize" type="submit">Send Message</MDBBtn>
+                        </form>
+                    </MDBCol>
+                </MDBRow>
             </MDBContainer>
         </Layout>
     )
