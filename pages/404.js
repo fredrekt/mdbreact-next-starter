19b981977cf6da3591b4de69935dd4426823ad3e
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-19 11:14:48 +0800

    added go home button

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,8 @@
-import { MDBCol, MDBContainer, MDBRow } from 'mdbreact'
+import { MDBBtn, MDBCol, MDBContainer, MDBRow } from 'mdbreact'
 import React from 'react'
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
+import Router from 'next/router';
 
 const MissingPage = () => {
     return (
@@ -14,6 +15,9 @@ const MissingPage = () => {
                         <div className="content text-center">
                             <h1 className="h1-responsive">Oooopssieee</h1>
                             <p className="lead">Page not found. Sorry for the Inconvenience, we're not able to find what you were looking for.</p>
+                            <MDBBtn color="dark" className="mx-0 text-capitalize" onClick={()=>Router.push('/')}>
+                                Go Home
+                            </MDBBtn> 
                         </div>
                     </MDBCol>
                     <MDBCol md="6" lg="6">
