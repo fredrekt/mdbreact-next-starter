551e1162d5a0d68af2a58d0b42e22c29d5010d3e
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-19 10:52:44 +0800

    added apollo ssr

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,18 +1,35 @@
-import { MDBContainer } from 'mdbreact'
-import React from 'react'
-import Layout from '../../components/Layout'
-import SEO from '../../components/SEO'
+import { MDBContainer } from 'mdbreact';
+import React from 'react';
+import Layout from '../../components/Layout';
+import SEO from '../../components/SEO';
+import Router from 'next/router';
+//apollo client
+import { withApollo } from '../../libs/apolloProvider';
+import { useQuery } from '@apollo/react-hooks';
+import { getAllBlogs } from '../../graphql/blog';
 
 const BlogsPage = () => {
+
+    const { data, loading, error } = useQuery(getAllBlogs)
+    if(loading) return <p>loading</p>
+    if(error){
+        Router.push('/404')
+    }
+
+    const { blogs } = data
+
     return (
         <Layout>
             <SEO title="Blogs" 
             desc="Collections of blogs about things and stuff. Read more here."/>
             <MDBContainer className="py-5 my-5">
                 <h1 className="h1-responsive">Blogs Page</h1>
+                <ul>
+                    {blogs && blogs.map(i => <li key={i.id}>{i.title}</li>)}
+                </ul>
             </MDBContainer>
         </Layout>
     )
 }
 
-export default BlogsPage
+export default withApollo({ ssr: true })(BlogsPage)
