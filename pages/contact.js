5d6235b812a372891a937f7c39cff2bc51f34f06
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-19 09:28:26 +0800

    contact page done

--- pages/contact.js
+++ pages/contact.js
@@ -22,6 +22,43 @@ const ContactPage = () => {
                     </MDBCol>
                 </MDBRow>
             </MDBContainer>
+            <section className="grey lighten-4 mt-5 py-5">
+                <MDBContainer className="py-5 my-5">
+                    <h1 className="display-3">Contact Us</h1>
+                    <MDBRow>
+                        <MDBCol md="4" lg="4">
+                            <div className="contact-details my-5">
+                                <h5 className="h5-responsive mb-3">Call</h5>
+                                <p className="m-0">Sales & Customer Service</p>
+                                <a href="#number" className="m-0 black-text">1-800-369-7469 x 4801</a>
+                            </div>
+
+                            <div className="contact-details my-5">
+                                <p className="m-0">Sales & Customer Service</p>
+                                <a href="#number" className="m-0 black-text">1-800-369-7469 x 4801</a>
+                            </div>
+                        </MDBCol>
+                        <MDBCol md="4" lg="4">
+                            <div className="contact-details my-5">
+                                <h5 className="h5-responsive mb-3">Email</h5>
+                                <a href="mailto:fredrickjohng7@gmail.com" className="m-0 black-text">fredrickjohng7@gmail.com</a>
+                            </div>
+
+                            <div className="contact-details my-5">
+                                <a href="mailto:fredrickjohng7@gmail.com" className="m-0 black-text">sample@email.com</a>
+                            </div>
+                        </MDBCol>
+                        <MDBCol md="4" lg="4">
+                            <div className="contact-details my-5">
+                                <MDBBtn className="mx-0 w-100 text-capitalize z-depth-0" color="dark">
+                                    Start Live Chat
+                                </MDBBtn>
+                                <p className="m-0 text-right grey-text">Mon to Fri 8:30am – 5pm (PST)</p>
+                            </div>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
+            </section>
         </Layout>
     )
 }
